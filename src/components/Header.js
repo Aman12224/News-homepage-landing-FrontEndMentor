d5953ce2204324a4df9c7b85017c3236182111b1
kidnamedfinger122 <amanchoudhari1224@gmail.com>
2023-08-24 21:48:44 +0530

    mobile site complited

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import bars from "../assets/images/icon-menu.svg";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button className="open-sidebar">
+            <img src={bars} alt="bars" />
+          </button>
+        </div>
+        <div className="nav-links">
+          <ul className="links-list">
+            <li className="single-list-item">
+              <a href="/">home</a>
+            </li>
+            <li className="single-list-item">
+              <a href="/">new</a>
+            </li>
+            <li className="single-list-item">
+              <a href="/">popular</a>
+            </li>
+            <li className="single-list-item">
+              <a href="/">trending</a>
+            </li>
+            <li className="single-list-item">
+              <a href="/">categories</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,72 @@
+import computer from "../assets/images/image-retro-pcs.jpg";
+import laptop from "../assets/images/image-top-laptops.jpg";
+import controller from "../assets/images/image-gaming-growth.jpg";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="wrapper">
+        <div className="hero-container">
+          <div className="hero"></div>
+          <h1 className="hero-title">The Bright Future of Web 3.0?</h1>
+          <p className="hero-desc">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className="hero-read-more-btn">Read more</button>
+        </div>
+        <div className="highlights-container">
+          <h2 className="highlights-container-title">New</h2>
+          <div className="single-highlight">
+            <h3>Hydrogen VS Electric Cars</h3>
+            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          </div>
+          <div className="single-highlight">
+            <h3>The Downsides of AI Artistry</h3>
+            <p>
+              What are the possible adverse effects of on-demand AI image
+              generation?
+            </p>
+          </div>
+          <div className="single-highlight">
+            <h3>Is VC Funding Drying Up?</h3>
+            <p>
+              Private funding by VC firms is down 50% YOY. We take a look at
+              what that means.
+            </p>
+          </div>
+        </div>
+
+        <div className="top-posts">
+          <div className="single-post">
+            <img src={computer} alt="" className="post-img" />
+            <div className="post-info">
+              <h1>01</h1>
+              <h3>Reviving Retro PCs</h3>
+              <p>What happens when old PCs are given modern upgrades?</p>
+            </div>
+          </div>
+          <div className="single-post">
+            <img src={laptop} alt="" className="post-img" />
+            <div className="post-info">
+              <h1>02</h1>
+              <h3>Top 10 Laptops of 2022</h3>
+              <p>Our best picks for various needs and budgets.</p>
+            </div>
+          </div>
+          <div className="single-post">
+            <img src={controller} alt="" className="post-img" />
+            <div className="post-info">
+              <h1>03</h1>
+              <h3>The Growth of Gaming</h3>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
